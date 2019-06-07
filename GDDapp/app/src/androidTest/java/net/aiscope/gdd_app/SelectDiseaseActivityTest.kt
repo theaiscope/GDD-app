b01c634164b074db1ee4708f92eb222a480a0d10
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2019-06-07 10:49:03 +0200

    [Lewis/Clara][Select disease] added entries to the disease spinner

--- GDDapp/app/src/androidTest/java/net/aiscope/gdd_app/SelectDiseaseActivityTest.kt
+++ GDDapp/app/src/androidTest/java/net/aiscope/gdd_app/SelectDiseaseActivityTest.kt
@@ -1,7 +1,27 @@
 package net.aiscope.gdd_app
 
-import org.junit.Assert.*
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.rule.ActivityTestRule
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.instanceOf
+import org.junit.Rule
+import org.junit.Test
+
 
 class SelectDiseaseActivityTest {
 
+    @get:Rule
+    var activityRule: ActivityTestRule<SelectDiseaseActivity> = ActivityTestRule(SelectDiseaseActivity::class.java)
+
+    @Test
+    fun spinnerShouldDisplayDiseasesList () {
+        Espresso.onView(ViewMatchers.withId(R.id.spinner_diseases))
+            .perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("Malaria"))).perform(click())
+        onData(allOf(`is`(instanceOf(String::class.java)), `is`("Tuberculosis"))).perform(click())
+    }
 }
