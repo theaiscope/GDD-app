7fca0605ab2e3b680b03d62e00921d66a4abafc4
Clara Gaset <clara.g@thoughtworks.com>
2019-06-12 12:14:35 +0200

    [Filipe/Clara][Select disease] Redirection to capture image activity

--- GDDapp/app/src/androidTest/java/net/aiscope/gdd_app/SelectDiseaseActivityTest.kt
+++ GDDapp/app/src/androidTest/java/net/aiscope/gdd_app/SelectDiseaseActivityTest.kt
@@ -3,8 +3,10 @@ package net.aiscope.gdd_app
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.rule.ActivityTestRule
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.instanceOf
@@ -15,7 +17,7 @@ import org.junit.Test
 class SelectDiseaseActivityTest {
 
     @get:Rule
-    var activityRule: ActivityTestRule<SelectDiseaseActivity> = ActivityTestRule(SelectDiseaseActivity::class.java)
+    val intentsTestRule = IntentsTestRule(SelectDiseaseActivity::class.java)
 
     @Test
     fun spinnerShouldDisplayDiseasesList () {
@@ -23,5 +25,13 @@ class SelectDiseaseActivityTest {
             .perform(click())
         onData(allOf(`is`(instanceOf(String::class.java)), `is`("Malaria"))).perform(click())
         onData(allOf(`is`(instanceOf(String::class.java)), `is`("Tuberculosis"))).perform(click())
+    }
+
+    @Test
+    fun shouldRedirectToCaptureImageActivity() {
+        Espresso.onView(ViewMatchers.withId(R.id.button_capture_image_select_disease))
+            .perform(click())
+
+        intended(hasComponent(CaptureImageActivity::class.java.name))
     }
 }
--- GDDapp/app/src/main/java/net/aiscope/gdd_app/SelectDiseaseActivity.kt
+++ GDDapp/app/src/main/java/net/aiscope/gdd_app/SelectDiseaseActivity.kt
@@ -1,12 +1,20 @@
 package net.aiscope.gdd_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class SelectDiseaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_disease)
+
+        val captureImageButton = findViewById<Button>(R.id.button_capture_image_select_disease)
+
+        captureImageButton.setOnClickListener {
+            startActivity(Intent(this, CaptureImageActivity::class.java))
+        }
     }
 }
