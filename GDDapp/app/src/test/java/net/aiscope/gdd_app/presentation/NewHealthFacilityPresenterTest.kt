556f37647a0e92a0931a8af8cee145c0155657a2
daferpi <daferpi@protonmail.ch>
2019-06-13 21:24:40 +0200

    add presenter test example

---
+++ GDDapp/app/src/test/java/net/aiscope/gdd_app/presentation/NewHealthFacilityPresenterTest.kt
@@ -0,0 +1,34 @@
+package net.aiscope.gdd_app.presentation
+
+import com.nhaarman.mockito_kotlin.verify
+import net.aiscope.gdd_app.R
+import net.aiscope.gdd_app.repository.HospitalRepository
+import net.aiscope.gdd_app.ui.newHealthFacility.NewHealthFacilityPresenter
+import net.aiscope.gdd_app.ui.newHealthFacility.NewHealthFacilityView
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class NewHealthFacilityPresenterTest {
+
+    @Mock
+    lateinit var view: NewHealthFacilityView
+
+    @Mock
+    lateinit var repository: HospitalRepository
+
+    @InjectMocks
+    lateinit var subject: NewHealthFacilityPresenter
+
+    @Test
+    fun shouldShowSaveMessageWhenSavedHospitalCallsOk() {
+        subject.saveHospital("test")
+
+        verify(view).showToast(R.string.confirmation_message_health_facility_saved)
+        verify(repository).store("test")
+    }
+}
