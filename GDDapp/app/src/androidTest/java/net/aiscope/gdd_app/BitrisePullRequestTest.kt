462f7d45235f86d74af5b04dacead1a56f61a98a
kaeff <hi@kaeff.net>
2019-06-26 18:27:01 +0200

    Test CI: Add a test that should fail the build

---
+++ GDDapp/app/src/androidTest/java/net/aiscope/gdd_app/BitrisePullRequestTest.kt
@@ -0,0 +1,15 @@
+package net.aiscope.gdd_app
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BitrisePullRequestTest {
+
+
+    @Test
+    fun shouldFailCi() {
+        assertEquals(true, false); }
+}
