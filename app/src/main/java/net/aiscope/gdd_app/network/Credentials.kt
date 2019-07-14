c7982fc370304f8cf8dfe5542859b2ff72aa6e5e
Jordi Coscolla <kozko2001@gmail.com>
2019-07-14 22:13:34 +0200

    ignoring credentials

--- app/src/main/java/net/aiscope/gdd_app/network/Credentials.kt
+++ app/src/main/java/net/aiscope/gdd_app/network/Credentials.kt
@@ -1,7 +1,7 @@
 package net.aiscope.gdd_app.network
 
 object Credentials {
-    val API_KEY = ""
+    val API_KEY = "__API_KEY__"
 
-    val API_SECRET = ""
+    val API_SECRET = "__API_SECRET__"
 }
