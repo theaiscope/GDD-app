b5922d0f6bdaf7bb8ebdcdfeaba00a9e2e783ecf
Eduard-Cristian Bolos <ebolos@thoughtworks.com>
2019-09-01 18:07:57 +0200

    Update dependencies

--- app/src/main/java/net/aiscope/gdd_app/application/GddApplication.kt
+++ app/src/main/java/net/aiscope/gdd_app/application/GddApplication.kt
@@ -1,25 +1,23 @@
 package net.aiscope.gdd_app.application
 
-import android.app.Activity
 import android.app.Application
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import androidx.work.WorkerFactory
-import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
+import dagger.android.HasAndroidInjector
 import net.aiscope.gdd_app.dagger.DaggerAppComponent
 import javax.inject.Inject
 
-class GddApplication : Application(), HasActivityInjector {
+class GddApplication : Application(), HasAndroidInjector {
 
     @Inject
-    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
     @Inject
     lateinit var workerFactory: WorkerFactory
 
-    override fun activityInjector(): AndroidInjector<Activity> = activityInjector
+    override fun androidInjector() = androidInjector
 
     override fun onCreate() {
         super.onCreate()
