56393976ee62bcb272558fa97c3c60e4c9cfc754
Eduard-Cristian Bolos <ebolos@thoughtworks.com>
2019-09-01 20:43:53 +0200

    Integrate Smartlook for screen recording

--- app/src/main/java/net/aiscope/gdd_app/application/GddApplication.kt
+++ app/src/main/java/net/aiscope/gdd_app/application/GddApplication.kt
@@ -1,11 +1,14 @@
 package net.aiscope.gdd_app.application
 
 import android.app.Application
+import android.view.TextureView
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import androidx.work.WorkerFactory
+import com.smartlook.sdk.smartlook.Smartlook
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import net.aiscope.gdd_app.BuildConfig
 import net.aiscope.gdd_app.dagger.DaggerAppComponent
 import javax.inject.Inject
 
@@ -26,5 +29,11 @@ class GddApplication : Application(), HasAndroidInjector {
 
         val conf = Configuration.Builder().setWorkerFactory(workerFactory).build()
         WorkManager.initialize(this, conf)
+
+        if (!BuildConfig.DEBUG) {
+            Smartlook.setupAndStartRecording(BuildConfig.SMARTLOOK_API_KEY)
+            Smartlook.registerBlacklistedClass(TextureView::class.java)
+            Smartlook.enableCrashlytics(true)
+        }
     }
 }
