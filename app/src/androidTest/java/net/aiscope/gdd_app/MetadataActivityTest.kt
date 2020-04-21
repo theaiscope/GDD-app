826054ae7114c23af57b14f6b88343371ed3633d
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2020-04-21 10:25:16 +0200

    [#67] Statically import Espresso functions to avoid repetition in `MetadataActivityTest`
    Co-authored-by: Magda Sbant <magda.sbant@thoughtworks.com>

--- app/src/androidTest/java/net/aiscope/gdd_app/MetadataActivityTest.kt
+++ app/src/androidTest/java/net/aiscope/gdd_app/MetadataActivityTest.kt
@@ -1,19 +1,21 @@
 package net.aiscope.gdd_app
 
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isChecked
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import net.aiscope.gdd_app.ui.metadata.MetadataActivity
 import org.hamcrest.CoreMatchers.anything
 import org.hamcrest.Matchers
 import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
-
 
 class MetadataActivityTest {
 
@@ -23,34 +25,34 @@ class MetadataActivityTest {
     @Test
     @Ignore("needs to have firebase auth current user stubbed")
     fun thickSmearTypeIsPreselected () {
-        Espresso.onView(ViewMatchers.withText(R.string.metadata_blood_smear_thick))
+        onView(withText(R.string.metadata_blood_smear_thick))
             .check(matches(isChecked()))
     }
 
     @Test
     @Ignore("Blood type is preselected")
     fun shouldShowErrorMessageIfNoBloodTypeSelected() {
-        Espresso.onView(ViewMatchers.withText(R.string.metadata_save_sample)).perform(click())
+        onView(withText(R.string.metadata_save_sample)).perform(click())
 
-        Espresso.onView(ViewMatchers.withText(R.string.metadata_invalid_form))
+        onView(withText(R.string.metadata_invalid_form))
             .inRoot(RootMatchers.withDecorView(Matchers.not(intentsMetadataTestRule.activity.window.decorView)))
-            .check(matches(ViewMatchers.isDisplayed()))
+            .check(matches(isDisplayed()))
     }
 
     @Test
     @Ignore("needs to have firebase auth current user stubbed")
     fun shouldStoreData() {
         // Select bloodType
-        Espresso.onView(ViewMatchers.withText(R.string.metadata_blood_smear_thin)).perform(click())
+        onView(withText(R.string.metadata_blood_smear_thin)).perform(click())
 //        onData(allOf(`is`(instanceOf(String::class.java)), `is`("thin"))).perform(click())
 
-        Espresso.onView(ViewMatchers.withId(R.id.metadata_species_spinner)).perform(click())
-        Espresso.onData(anything()).atPosition(1).perform(click())
+        onView(ViewMatchers.withId(R.id.metadata_species_spinner)).perform(click())
+        onData(anything()).atPosition(1).perform(click())
 
-        Espresso.onView(ViewMatchers.withId(R.id.metadata_stage_spinner)).perform(click())
-        Espresso.onData(anything()).atPosition(1).perform(click())
+        onView(ViewMatchers.withId(R.id.metadata_stage_spinner)).perform(click())
+        onData(anything()).atPosition(1).perform(click())
 
         // Save button click
-        Espresso.onView(ViewMatchers.withText("SAVE SAMPLE")).perform(click())
+        onView(withText("SAVE SAMPLE")).perform(click())
     }
 }
