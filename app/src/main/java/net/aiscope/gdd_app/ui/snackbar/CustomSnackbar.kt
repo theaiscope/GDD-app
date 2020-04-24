20b50003134d7240121e7f04e8a347c3a7d063c7
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2020-04-24 11:00:54 +0200

    [#67] Refactor CustomSnackbar

--- app/src/main/java/net/aiscope/gdd_app/ui/snackbar/CustomSnackbar.kt
+++ app/src/main/java/net/aiscope/gdd_app/ui/snackbar/CustomSnackbar.kt
@@ -15,7 +15,6 @@ class SimpleCustomSnackbar (
     content: CustomSnackbarView
 ) : BaseTransientBottomBar<SimpleCustomSnackbar>(parent, content, content) {
 
-
     init {
         getView().setBackgroundColor(
             ContextCompat.getColor(
@@ -30,62 +29,58 @@ class SimpleCustomSnackbar (
 
         fun make(
             view: View,
-            message: String, duretion: Int,
-            listener: View.OnClickListener?, icon: Int, action_lable: String?, bg_color: Int
-        ): SimpleCustomSnackbar? {
+            message: String, duration: Int,
+            listener: View.OnClickListener?, icon: Int, actionLabel: String?, backgroundColor: Int
+        ): SimpleCustomSnackbar {
 
-            // First we find a suitable parent for our custom view
             val parent = findSuitableParent(view) ?: throw IllegalArgumentException(
                 "No suitable parent found from the given view. Please provide a valid view."
             )
 
-            // We inflate our custom view
             val customView = LayoutInflater.from(view.context).inflate(
                 R.layout.custom_view_inflation,
                 parent,
                 false
             ) as CustomSnackbarView
-            // We create and return our Snackbar
-            customView.tv_message.text = message
-            action_lable?.let {
-                customView.tvAction.text = action_lable
-                customView.tvAction.setOnClickListener {
-                    listener?.onClick(customView.tvAction)
+
+            with(customView) {
+                tv_message.text = message
+                actionLabel?.let {
+                    tvAction.text = actionLabel
+                    tvAction.setOnClickListener {
+                        listener?.onClick(customView.tvAction)
+                    }
                 }
+                imLeft.setImageResource(icon)
+                layRoot.setBackgroundColor(backgroundColor)
             }
-            customView.imLeft.setImageResource(icon)
-            customView.layRoot.setBackgroundColor(bg_color)
 
             return SimpleCustomSnackbar(
                 parent,
                 customView
-            ).setDuration(duretion)
+            ).setDuration(duration)
         }
 
-        private fun findSuitableParent(parentView: View?): ViewGroup? {
-            var view = parentView
+        private fun findSuitableParent(view: View?): ViewGroup? {
             var fallback: ViewGroup? = null
-            do {
-                when (view) {
-                    is CoordinatorLayout -> {
+
+            when (view) {
+                is CoordinatorLayout -> {
+                    return view
+                }
+                is FrameLayout -> {
+                    if (view.id == android.R.id.content) {
                         return view
-                    }
-                    is FrameLayout -> {
-                        if (view.id == android.R.id.content) {
-                            return view
-                        } else {
-                            fallback = view
-                        }
+                    } else {
+                        fallback = view
                     }
                 }
+            }
 
-                if (view != null) {
-                    val parent = view.parent
-                    view = if (parent is View) parent else null
-                }
-            } while (view != null)
+            val parentView = view?.parent as View?
 
-            return fallback
+            return if(parentView == null) fallback else findSuitableParent(parentView)
+
         }
 
     }
