4df772079e816e6367ed3f3735405bbd3c9f0343
Joris Borsboom <jorisborsboom@gmail.com>
2020-05-08 17:38:49 +0200

    Import asserts

--- app/src/androidTest/java/net/aiscope/gdd_app/model/SampleTest.kt
+++ app/src/androidTest/java/net/aiscope/gdd_app/model/SampleTest.kt
@@ -2,7 +2,9 @@ package net.aiscope.gdd_app.model
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import net.aiscope.gdd_app.network.toDto
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,10 +20,10 @@ class SampleTest {
         //This is the DTO for sending to firebase rather than storing locally
         val dto = sample.toDto()
 
-        Assert.assertEquals(dto.id, "_id")
-        Assert.assertEquals(dto.healthFacility,"facility")
-        Assert.assertEquals(dto.microscopist, "microscopist")
-        Assert.assertTrue(dto.appVersion.matches(Regex("""^\d+\.\d+\.\d+ \([a-zA-Z0-9]+\)""")))
-        Assert.assertNotNull(dto.device)
+        assertEquals(dto.id, "_id")
+        assertEquals(dto.healthFacility,"facility")
+        assertEquals(dto.microscopist, "microscopist")
+        assertTrue(dto.appVersion.matches(Regex("""^\d+\.\d+\.\d+ \([a-zA-Z0-9]+\)""")))
+        assertNotNull(dto.device)
     }
 }
