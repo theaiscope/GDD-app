0f225cab89a668715cd248d560a7959e9c67f2e4
Raul Oliver <raul.oliver@thoughtworks.com>
2020-05-20 19:39:07 +0200

    [104] Addresses suggestions of PR-32

--- app/src/androidTest/java/net/aiscope/gdd_app/model/SampleTest.kt
+++ app/src/androidTest/java/net/aiscope/gdd_app/model/SampleTest.kt
@@ -2,6 +2,8 @@ package net.aiscope.gdd_app.model
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import net.aiscope.gdd_app.network.toDto
+import net.aiscope.gdd_app.repository.MicroscopeQualityDto
+import net.aiscope.gdd_app.repository.SamplePreparationDto
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
@@ -15,16 +17,47 @@ class SampleTest {
 
     @Test
     fun shouldFillExpectedFields() {
+        val samplePreparation = SamplePreparation(
+            WaterType.TAP,
+            usesGiemsa = true,
+            giemsaFP = false,
+            usesPbs = true,
+            usesAlcohol = false,
+            reusesSlides = true
+        )
+        val microscopeQuality = MicroscopeQuality(
+            true,
+            1240
+        )
         val sample = Sample("_id",
             "facility",
-            "microscopist")
+            "microscopist",
+            preparation = samplePreparation,
+            microscopeQuality = microscopeQuality
+        )
 
         //This is the DTO for sending to firebase rather than storing locally
         val dto = sample.toDto()
 
-        assertEquals(dto.id, "_id")
-        assertEquals(dto.healthFacility,"facility")
-        assertEquals(dto.microscopist, "microscopist")
+        val expectedSamplePreparationDto = SamplePreparationDto(
+            WaterType.TAP.id,
+            usesGiemsa = true,
+            giemsaFP = false,
+            usesPbs = true,
+            usesAlcohol = false,
+            reusesSlides = true
+        )
+
+        val expectedMicroscopeQualityDto = MicroscopeQualityDto(
+            true,
+            1240
+        )
+
+        assertEquals("_id", dto.id)
+        assertEquals("facility", dto.healthFacility)
+        assertEquals("microscopist", dto.microscopist)
+        assertEquals(expectedSamplePreparationDto, dto.preparation)
+        assertEquals(expectedMicroscopeQualityDto, dto.microscopeQuality)
         assertTrue(dto.appVersion.matches(VERSION_REGEX))
         assertNotNull(dto.device)
     }
