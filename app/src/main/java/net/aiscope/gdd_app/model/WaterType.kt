41143605a1b3f44dcfc1492067cace8d30f0abfb
Raul Oliver <raul.oliver@thoughtworks.com>
2020-05-20 15:30:02 +0200

    [104] Suppresses Detekt debt

--- app/src/main/java/net/aiscope/gdd_app/model/WaterType.kt
+++ app/src/main/java/net/aiscope/gdd_app/model/WaterType.kt
@@ -1,8 +1,9 @@
 package net.aiscope.gdd_app.model
 
+@Suppress("MagicNumber") // these are IDs
 enum class WaterType(val id: Int) {
     DISTILLED(1),
     BOTTLED(2),
     TAP(3),
-    WELL(4),
+    WELL(4)
 }
