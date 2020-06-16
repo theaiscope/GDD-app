57aa06535af0819059c9946fce2ff00c84f1bfc5
Eduard-Cristian Boloș <eduardb@users.noreply.github.com>
2020-06-16 12:17:55 +0200

    Smoother drawing + points (#44)

--- app/src/main/java/net/aiscope/gdd_app/ui/mask/customview/MaskLayer.kt
+++ app/src/main/java/net/aiscope/gdd_app/ui/mask/customview/MaskLayer.kt
@@ -1,6 +1,5 @@
 package net.aiscope.gdd_app.ui.mask.customview
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
@@ -10,16 +9,11 @@ import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
 import android.os.Parcelable
 import android.util.Size
-import android.view.ViewConfiguration
 import androidx.core.graphics.withMatrix
 import java.util.*
-import kotlin.math.abs
 
 @Suppress("TooManyFunctions")
-class MaskLayer(
-    private val context: Context,
-    private val imageMatrix: Matrix
-) {
+class MaskLayer(private val imageMatrix: Matrix) {
     companion object {
         private const val ALPHA_OPAQUE = 0xFF
         private const val MASK_PAINT_OPACITY = .8
@@ -141,10 +135,16 @@ class MaskLayer(
             val (path, paint) = pathsAndPaints[i]
             val paintReviewed =
                 if (removeAlpha) Paint(paint).apply { alpha = ALPHA_OPAQUE } else paint
-            canvas.drawPath(path, paintReviewed)
+            path.draw(canvas, paintReviewed)
         }
-        currentPath?.let {
-            canvas.drawPath(it, currentPaint)
+        currentPath?.draw(canvas, currentPaint)
+    }
+
+    private fun PointToPointPath.draw(canvas: Canvas, paint: Paint) {
+        if (!hasMultiplePoints()) {
+            canvas.drawPoint(firstPoint.first, firstPoint.second, paint)
+        } else {
+            canvas.drawPath(this, paint)
         }
     }
 
@@ -204,23 +204,12 @@ class MaskLayer(
     }
 
     fun drawMove(x: Float, y: Float) {
-        currentPath?.run {
-            val (latestX, latestY) = latestPoint
-            val dX = abs(x - latestX)
-            val dY = abs(y - latestY)
-            val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop / currentScale
-            if (dX >= touchTolerance || dY >= touchTolerance) {
-                quadTo(x, y)
-            }
-        }
+        currentPath?.quadTo(x, y)
     }
 
     fun drawEnd() {
         currentPath?.run {
-            val visibleChange: Boolean =
-                if (isCurrentModeDraw()) hasMultiplePoints()
-                else !latestChangeBitmap.sameAs(currentStateBitmap)
-            if (visibleChange) {
+            if (!latestChangeBitmap.sameAs(currentStateBitmap)) {
                 keepLatestChangeBitmap()
                 flushPendingUndos()
                 pathsAndPaints.add(PathAndPaint(this, currentPaint))
