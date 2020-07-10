f4902bb55b42f9d55bcf5be4bf0f13e5d74e3133
Joris Borsboom <jorisborsboom@gmail.com>
2020-07-10 14:58:28 +0200

    Remove a few unused imports

--- app/src/main/java/net/aiscope/gdd_app/dagger/PrivacyPolicyModule.kt
+++ app/src/main/java/net/aiscope/gdd_app/dagger/PrivacyPolicyModule.kt
@@ -2,9 +2,7 @@ package net.aiscope.gdd_app.dagger
 
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import net.aiscope.gdd_app.ui.policy.PrivacyPolicyActivity
-import net.aiscope.gdd_app.ui.policy.PrivacyPolicyPresenter
 import net.aiscope.gdd_app.ui.policy.PrivacyPolicyView
 
 @Module
