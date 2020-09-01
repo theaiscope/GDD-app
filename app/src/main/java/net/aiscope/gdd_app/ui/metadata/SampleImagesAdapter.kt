97d28e798efff988a571d31248808427b69c1260
Clara Gaset <claragaset4@gmail.com>
2020-09-01 11:00:20 +0200

    [154] Differentiaties masked images

--- app/src/main/java/net/aiscope/gdd_app/ui/metadata/SampleImagesAdapter.kt
+++ app/src/main/java/net/aiscope/gdd_app/ui/metadata/SampleImagesAdapter.kt
@@ -1,21 +1,15 @@
 package net.aiscope.gdd_app.ui.metadata
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import net.aiscope.gdd_app.R
-import net.aiscope.gdd_app.extensions.writeToFile
 import net.aiscope.gdd_app.ui.util.BitmapReader
-import net.aiscope.gdd_app.ui.util.MinimumSizeDownSampling
 import java.io.File
 
 class SampleImagesAdapter(
@@ -32,7 +26,16 @@ class SampleImagesAdapter(
         val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
         return when (viewType) {
             R.layout.item_metadata_add_image -> AddImageViewHolder(view, onAddImageClicked)
-            R.layout.item_metadata_sample_image -> ImageViewHolder(view as ImageView, uiScope, onImageClicked)
+            R.layout.item_metadata_sample_image -> ImageViewHolder(
+                view as ImageView,
+                uiScope,
+                onImageClicked
+            )
+            R.layout.item_metadata_sample_image_with_mask -> ImageWithMaskViewHolder(
+                view,
+                uiScope,
+                onImageClicked
+            )
             else -> throw IllegalArgumentException("View type $viewType not known")
         }
     }
@@ -40,6 +43,8 @@ class SampleImagesAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
             is AddImageViewHolder -> {}
+            is ImageWithMaskViewHolder ->
+                holder.bind(images[position - 1], masks[position - 1])
             is ImageViewHolder ->
                 holder.bind(images[position - 1], masks[position - 1])
             else -> throw IllegalArgumentException("View holder ${holder.javaClass} not known")
@@ -49,7 +54,10 @@ class SampleImagesAdapter(
     override fun getItemViewType(position: Int): Int {
         return when (position) {
             0 -> R.layout.item_metadata_add_image
-            else -> R.layout.item_metadata_sample_image
+            else -> {
+                if (masks[position - 1].length().toInt() > 4885) R.layout.item_metadata_sample_image_with_mask
+                else R.layout.item_metadata_sample_image
+            }
         }
     }
 
@@ -103,6 +111,36 @@ private class ImageViewHolder(
                 itemView.context.cacheDir
             )
             itemView.setImageBitmap(bitmap)
+        }
+    }
+}
+
+private class ImageWithMaskViewHolder(
+    view: View, private val uiScope: CoroutineScope, onImageClicked: (File, File) -> Unit
+) : RecyclerView.ViewHolder(view) {
+    private lateinit var imageFile: File
+    private lateinit var maskFile: File
+
+    init {
+        itemView.setOnClickListener { onImageClicked(imageFile, maskFile) }
+    }
+
+    fun bind(image: File, mask: File) {
+        imageFile = image
+        maskFile = mask
+
+        val img: ImageView = itemView.findViewById(R.id.sample_image)
+
+        (itemView.tag as? Job)?.cancel()
+        itemView.tag = uiScope.launch {
+            img.setImageBitmap(null)
+            val bitmap = BitmapReader.decodeSampledBitmapAndCache(
+                image,
+                img.context.resources.getDimensionPixelSize(R.dimen.sample_image_thumbnail_width),
+                img.context.resources.getDimensionPixelSize(R.dimen.sample_image_thumbnail_height),
+                img.context.cacheDir
+            )
+            img.setImageBitmap(bitmap)
         }
     }
 }
