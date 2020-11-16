89c851997d8dce86154345bf8366ea191196a31a
Joris Borsboom <jorisborsboom@gmail.com>
2020-11-16 11:20:19 +0100

    Make sure to only take 1 picture

--- app/src/main/java/net/aiscope/gdd_app/ui/capture/CaptureImagePresenter.kt
+++ app/src/main/java/net/aiscope/gdd_app/ui/capture/CaptureImagePresenter.kt
@@ -6,20 +6,26 @@ import timber.log.Timber
 
 class CaptureImagePresenter(
     val view: CaptureImageView,
-    val repository: SampleRepository
+    val repository: SampleRepository,
+    var receivedImageCapture: Boolean = false
 ) {
 
     fun handleCaptureImageButton(imageName: String) {
-        Timber.tag("Taking Photo").d("button pressed")
-        view.takePhoto(imageName) { file ->
-            Timber.tag("Taking Photo").d(file?.absolutePath)
-            if (file == null) {
-                view.notifyImageCouldNotBeTaken()
-            } else {
-                val sample = repository.current().addNewlyCapturedImage(file)
-                repository.store(sample)
+        if (receivedImageCapture){
+            Timber.tag("Taking Photo").d("Already processing photo")
+        } else {
+            receivedImageCapture = true
+            Timber.tag("Taking Photo").d("button pressed")
+            view.takePhoto(imageName) { file ->
+                Timber.tag("Taking Photo").d(file?.absolutePath)
+                if (file == null) {
+                    view.notifyImageCouldNotBeTaken()
+                } else {
+                    val sample = repository.current().addNewlyCapturedImage(file)
+                    repository.store(sample)
 
-                view.goToMask(sample.disease, file.absolutePath, sample.nextMaskName())
+                    view.goToMask(sample.disease, file.absolutePath, sample.nextMaskName())
+                }
             }
         }
     }
