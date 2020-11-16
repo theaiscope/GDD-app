0f200b7742a1a6382a9c470d1bd590f0c7dfd401
Joris Borsboom <jorisborsboom@gmail.com>
2020-11-16 11:37:26 +0100

    Suggested change

--- app/src/main/java/net/aiscope/gdd_app/ui/capture/CaptureImagePresenter.kt
+++ app/src/main/java/net/aiscope/gdd_app/ui/capture/CaptureImagePresenter.kt
@@ -13,19 +13,19 @@ class CaptureImagePresenter(
     fun handleCaptureImageButton(imageName: String) {
         if (receivedImageCapture){
             Timber.tag("Taking Photo").d("Already processing photo")
-        } else {
-            receivedImageCapture = true
-            Timber.tag("Taking Photo").d("button pressed")
-            view.takePhoto(imageName) { file ->
-                Timber.tag("Taking Photo").d(file?.absolutePath)
-                if (file == null) {
-                    view.notifyImageCouldNotBeTaken()
-                } else {
-                    val sample = repository.current().addNewlyCapturedImage(file)
-                    repository.store(sample)
+            return
+        }
+        receivedImageCapture = true
+        Timber.tag("Taking Photo").d("button pressed")
+        view.takePhoto(imageName) { file ->
+            Timber.tag("Taking Photo").d(file?.absolutePath)
+            if (file == null) {
+                view.notifyImageCouldNotBeTaken()
+            } else {
+                val sample = repository.current().addNewlyCapturedImage(file)
+                repository.store(sample)
 
-                    view.goToMask(sample.disease, file.absolutePath, sample.nextMaskName())
-                }
+                view.goToMask(sample.disease, file.absolutePath, sample.nextMaskName())
             }
         }
     }
