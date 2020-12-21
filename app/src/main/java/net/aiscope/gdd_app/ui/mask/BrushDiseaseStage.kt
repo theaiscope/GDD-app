056503538a5454758bd6b200e255704822378fea
Eduard-Cristian Boloș <eduardb@users.noreply.github.com>
2020-12-21 11:45:08 +0100

    Migrate to kotlin-parcelize plugin (#125)
    * Migrate to kotlin-parcelize plugin
    
    * Upgrade Gradle wrapper

--- app/src/main/java/net/aiscope/gdd_app/ui/mask/BrushDiseaseStage.kt
+++ app/src/main/java/net/aiscope/gdd_app/ui/mask/BrushDiseaseStage.kt
@@ -1,7 +1,7 @@
 package net.aiscope.gdd_app.ui.mask
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class BrushDiseaseStage(val id: Int, val name: String, val maskColor: Int) : Parcelable {
