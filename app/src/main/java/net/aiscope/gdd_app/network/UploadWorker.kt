6b6469825c5672dda91dce4146b21db6c7c40f0a
Joris Borsboom <jorisborsboom@gmail.com>
2024-02-01 15:50:08 +0100

    Extra debug logging

--- app/src/main/java/net/aiscope/gdd_app/network/UploadWorker.kt
+++ app/src/main/java/net/aiscope/gdd_app/network/UploadWorker.kt
@@ -15,15 +15,19 @@ class UploadWorker constructor(
 
     override suspend fun doWork(): Result {
         return when (val id = inputData.getString("sample_id")) {
-            null -> Result.failure()
+            null -> {
+                Timber.e("Could not find sample id for input data", inputData)
+                Result.failure()
+            }
             else -> {
                 try {
                     val sample = repo.load(id)
 
                     storage.upload(sample)
+                    Timber.i("Successfully uploaded for ", id)
                     Result.success()
                 } catch (@Suppress("TooGenericExceptionCaught") error: Throwable) {
-                    Timber.e(error, "An error occurred when doing work")
+                    Timber.e(error, "An error occurred when doing work ", id)
                     Result.retry()
                 }
             }
